d5484323c92088140285fa56e0c5c1ad51283dd7
神楽坂雅詩 <cyscxchope@gmail.com>
2015-12-07 12:31:37 +0800

    优化命令行显示程序

--- YashiMsgViewer/Program.cs
+++ YashiMsgViewer/Program.cs
@@ -12,10 +12,13 @@ namespace YashiMsgViewer
     {
         static void Main(string[] args)
         {
-            Console.Title = "Yashi application message viewer";
-            if (args.Length > 0 && args[0] == "-viewer")
+            String name = "Yashi application message viewer";
+            Console.Title = name;
+            if (args.Length > 0 && (args[0] == "-viewer" || args[0] == "--viewer" || args[0] == "/viewer"))
             {
+                Console.Clear();
                 bool run = true;
+                bool echo = true;
                 try
                 {
                     while (run)
@@ -25,9 +28,17 @@ namespace YashiMsgViewer
                         {
                             break;
                         }
-                        else if (cmd == "cls")
+                        else if (cmd == "cls" || cmd == "clear")
                         {
                             Console.Clear();
+                        }
+                        else if (cmd == "echo on")
+                        {
+                            echo = true;
+                        }
+                        else if (cmd == "echo off")
+                        {
+                            echo = false;
                         }
                         else if (cmd.Length > 6 && cmd.Substring(0, 6) == "title ")
                         {
@@ -35,19 +46,40 @@ namespace YashiMsgViewer
                         }
                         else
                         {
-                            Console.WriteLine(cmd);
+                            if (echo)
+                            {
+                                Console.WriteLine(cmd);
+                            }
                         }
                     }
+                    Console.Clear();
                 }
                 catch (Exception e)
                 {
-
+                    Console.WriteLine("\n" + name + "错误：" + e.Message.ToString());
                 }
+            }
+            else if (args.Length > 0 && (args[0] == "-help" || args[0] == "--help" || args[0] == "/help" || args[0] == "/?"))
+            {
+                Console.WriteLine("用法： <thisapp> [ --viewer | --help ]");
+                Console.WriteLine("没有参数：显示提示退出。");
+                Console.WriteLine("viewer：输入模式开启。");
+                Console.WriteLine("help：显示此帮助。");
+                Console.WriteLine("语法：");
+                Console.WriteLine("exit：退出此程序。");
+                Console.WriteLine("echo [ on | off ]：显示输入回馈。");
+                Console.WriteLine("cls / clear：清除输出。");
+                Console.WriteLine("title：设置标题栏显示文字。");
+                Console.WriteLine("其他：录入信息。");
+                Console.WriteLine("请按任意键退出...");
+                Console.ReadKey();
             }
             else
             {
-                Console.WriteLine(" \n     这个程序是雅诗应用中的一部分，不能单独运行。\n \n          请按任意键退出...");
+                Console.WriteLine("这个程序是雅诗应用中的一部分，不能单独运行。");
+                Console.WriteLine("请按任意键退出...");
                 Console.ReadKey();
+                Console.Clear();
             }
         }
     }
