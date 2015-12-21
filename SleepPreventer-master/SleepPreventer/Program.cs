927a79d457015709fe602925c5cec169a6afd9d3
神楽坂雅詩 <cyscxchope@gmail.com>
2015-12-21 23:08:41 +0800

    引入h46incon/SleepPreventer

---
+++ SleepPreventer-master/SleepPreventer/Program.cs
@@ -0,0 +1,40 @@
+﻿//程序作者：h46incon。
+//神楽坂雅詩加入了少量修改。
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SleepPreventer
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //new MyNotifyIcon();
+            //Application.Run();
+            LidCloseAwakeKeeper.CheckLogError();
+            bool createNew;
+
+            using (System.Threading.Mutex mutex = new System.Threading.Mutex(true, Application.ProductName, out createNew))
+            {
+
+                if (createNew)
+                {
+                    Application.Run(new Form1());
+                }
+                else
+                {
+                    MessageBox.Show("程序已在运行");
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
